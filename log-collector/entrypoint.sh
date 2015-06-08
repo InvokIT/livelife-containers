0a555d6b515f9213784ee7146244e5cef22de952
Qvazar <sebjorg@gmail.com>
2015-06-08 17:54:47 +0200

    cloudwatch logging

--- log-collector/entrypoint.sh
+++ log-collector/entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-$AWS_METADATAURL="http://169.254.169.254/2015-01-05/meta-data"
+$AWS_METADATAURL="http://169.254.169.254/2014-11-05/meta-data"
 $AWS_INSTANCEID=$(curl ${AWS_METADATAURL}/instance-id)
-$AWS_DOMAIN=$(curl ${AWS_METADATAURL}/services/domain)
 $AWS_AVZONE=$(curl ${AWS_METADATAURL}/placement/availability-zone)
 
 exec /usr/local/bin/supervisord

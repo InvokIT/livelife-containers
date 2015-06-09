d74f180f775645d728e991c0471139b634d181eb
Qvazar <sebjorg@gmail.com>
2015-06-09 14:10:45 +0200

    aws logging in rtmp container

--- log-collector/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-$AWS_METADATAURL="http://169.254.169.254/2014-11-05/meta-data"
-$AWS_INSTANCEID=$(curl ${AWS_METADATAURL}/instance-id)
-$AWS_AVZONE=$(curl ${AWS_METADATAURL}/placement/availability-zone)
-
-exec /usr/local/bin/supervisord
--- rtmp/entrypoint.sh
+++ rtmp/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 mkdir -p /shared/{hls,dash,rec} || exit 1
-exec /usr/sbin/nginx "$@" 2>> /var/log/nginx/stderr.log
+exec /usr/sbin/nginx "$@"

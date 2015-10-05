ee8be664df671885b9379b4dc81de7215fe6e42d
Qvazar <sebjorg@gmail.com>
2015-10-05 18:34:07 +0200

    Fixed build-to-registry

--- build-to-registry.sh
+++ build-to-registry.sh
@@ -3,15 +3,16 @@
 set -e
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-REGISTRY=$1
+REGISTRY="$1"
 
 #docker run -d --name docker-registry -v ${DIR}/docker-registry/volume:/var/lib/registry -p 5000:5000 registry:2
 
 UNITS=("rtmp")
 
 for i in "${UNITS[@]}"
-	docker build -t $(REGISTRY)/${i} ${DIR}/${i}/
-	docker push $(REGISTRY)/${i}
+do
+	exec docker build -t ${REGISTRY}/${i} ${DIR}/${i}/
+	exec docker push ${REGISTRY}/${i}
 done
 
 

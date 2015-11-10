2ea435216787150203a4930d25979e4bde596cf4
Qvazar <sebjorg@gmail.com>
2015-11-10 22:17:16 +0100

    Begin rtmp controller

--- google/create-main-cluster.sh
+++ google/create-main-cluster.sh
@@ -2,7 +2,7 @@
 exec gcloud container --project "steady-cat-112112" clusters create \
 "main-cluster" \
 --zone "europe-west1-b" \
---machine-type "g1-small" \
+--machine-type "n1-standard-1" \
 --network "default" \
 --enable-cloud-logging \
 --enable-cloud-monitoring
---
+++ rtmp-controller/log.coffee
@@ -0,0 +1,9 @@
+log = require "log4js"
+###
+if process.env.NODE_ENV is "debug"
+	log.setLevel "all"
+###
+
+
+
+module.exports = log.getLogger "rtmp-monitor"

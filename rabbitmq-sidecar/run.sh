5d00a136ae4ab0499fa4aba0812977af4dfb77b5
Qvazar <sebjorg@gmail.com>
2015-12-08 16:12:41 +0100

    Delete rabbitmq-sidecar

--- rabbitmq-sidecar/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-
-docker build -t qvazar/rabbitmq-sidecar ${DIR}
-docker run -p 5672:5672 -p 4369:4369 -p 25672:25672 -p 15672:15672 qvazar/rabbitmq-sidecar
